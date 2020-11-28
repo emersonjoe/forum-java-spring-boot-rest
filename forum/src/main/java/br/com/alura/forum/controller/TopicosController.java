98f3241e395946769c55e597f55600350238e932
Emerson Santos <emerson.santos@asbpm.com.br>
2020-11-28 16:30:17 -0300

    topico by id

--- forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -58,5 +59,14 @@ public class TopicosController {
 		
 		URI  uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
 		return ResponseEntity.created(uri).body(new TopicoDto(topico));
+	}
+	
+	@GetMapping("/{id}")
+	public TopicoDto detalhar(@PathVariable Long id) {
+		
+		Topico topico = topicoRepository.getOne(id);
+		
+		return new TopicoDto(topico);
+		
 	}
 }
